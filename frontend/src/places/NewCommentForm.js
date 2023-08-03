39b0dd5920a175038aa0a1dcd59971f8d5da3556
Evan <evanlowell01@gmail.com>
2023-08-03 19:46:22 -0400

    updated session authentication branch

--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router"
 
 function NewCommentForm({ place, onSubmit }) {
 
-    const [authors, setAuthors] = useState([])
+    //const [authors, setAuthors] = useState([])
 
     const [comment, setComment] = useState({
         content: '',
@@ -37,6 +37,12 @@ function NewCommentForm({ place, onSubmit }) {
         })
     }
 
+    const { currentUser } = useContext(CurrentUser)
+
+    if(!currentUser) {
+        return <p>You must be logged in to leave a rant or rave.</p>
+    }
+
     return (
         <form onSubmit={handleSubmit}>
             <div className="row">
@@ -53,12 +59,12 @@ function NewCommentForm({ place, onSubmit }) {
                 </div>
             </div>
             <div className="row">
-                <div className="form-group col-sm-4">
+                {/*<div className="form-group col-sm-4">
                     <label htmlFor="state">Author</label>
                     <select className="form-control" value={comment.authorId} onChange={e => setComment({ ...comment, authorId: e.target.value })}>
                         {authorOptions}
                     </select>
-                </div>
+    </div>*/}
                 <div className="form-group col-sm-4">
                     <label htmlFor="stars">Star Rating</label>
                     <input
