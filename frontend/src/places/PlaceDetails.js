bebfd0c5e747701cafdbcb8c2accbfdce9b3bf77
Evan <evanlowell01@gmail.com>
2023-08-03 20:20:10 -0400

    updated session branch

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -100,6 +100,20 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+	if (currentUser?.role === 'admin') {
+		placeActions = (
+			<>
+				<a className="btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>
+				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
+
 
 	return (
 		<main>
